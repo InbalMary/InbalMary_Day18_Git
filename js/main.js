963a3d55224f772a83b2c14888b7cbbf583d2868
InbalMary <inbalby10@gmail.com>
2024-12-24 14:31:59 +0200

    Limit ball size up tp 400

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ var gSize = 100
 
 function onBallClick(elBall){
     gSize += 50
+
+    if(gSize > 400){
+        gSize = 100
+    }
     console.log(elBall)
     elBall.style.height = gSize + 'px'
     elBall.style.width = gSize + 'px'
