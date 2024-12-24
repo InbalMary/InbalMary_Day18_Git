1bb27fb440c2d68a2895d02a2fb084783b321370
InbalMary <inbalby10@gmail.com>
2024-12-24 17:00:07 +0200

    Add sixth ball

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 'use strict'
 
 var gSize = 100
+var gOrig1Color = 'aquamarine'
+var gOrig2Color = 'purple'
+
+var gHoverTimeout
+var gEnterCounter = 0
 
 function onBallClick(elBall, maxDiameter) {
     var color = getRandomColor()
@@ -47,16 +52,16 @@ function onBall4Click() {
     var size1 = parseFloat(elBall1.style.height)
     var size2 = parseFloat(elBall2.style.height)
 
-    if (size1 - rand < 100){
-    } 
+    if (size1 - rand < 100) {
+    }
     else {
         elBall1.style.height = size1 - rand + 'px'
         elBall1.style.width = size1 - rand + 'px'
         elBall1.innerText = size1 - rand
     }
 
-    if (size2 - rand < 100){
-    } 
+    if (size2 - rand < 100) {
+    }
     else {
         elBall2.style.height = size2 - rand + 'px'
         elBall2.style.width = size2 - rand + 'px'
@@ -64,7 +69,47 @@ function onBall4Click() {
     }
 }
 
-function onBall5Click(){
+function onBall5Click() {
     var elBody = document.querySelector('body')
     elBody.style.backgroundColor = getRandomColor()
+}
+
+function onBall6Click() {
+    var elBall1 = document.querySelector('.ball1')
+    var elBall2 = document.querySelector('.ball2')
+
+    var size1 = parseFloat(elBall1.style.height)
+    var size2 = parseFloat(elBall2.style.height)
+
+    elBall1.style.backgroundColor = gOrig1Color
+    elBall2.style.backgroundColor = gOrig2Color
+
+    console.log('gOrig1Color', gOrig1Color)
+    elBall1.style.height = 100 + 'px'
+    elBall1.style.width = 100 + 'px'
+    elBall1.innerText = 100
+
+    elBall2.style.height = 100 + 'px'
+    elBall2.style.width = 100 + 'px'
+    elBall2.innerText = 100
+
+    var elBody = document.querySelector('body')
+    elBody.style.backgroundColor = 'black'
+}
+
+function onMouseenter() {
+    gEnterCounter++
+    gHoverTimeout = setTimeout(() => {
+        onBall6Click()
+    }, 2000);
+    console.log('gEnterCounter', gEnterCounter)
+    if(gEnterCounter > 10) {
+        gEnterCounter = 0
+        onBall6Click()
+        onMouseleave()
+    }
+}
+
+function onMouseleave() {
+    clearInterval(gHoverTimeout)
 }
